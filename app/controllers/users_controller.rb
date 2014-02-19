5418f16a610601d3cc7e1aa2340f2b1a60744cd4
Ryan Ricks <ryanricks@berkeley.edu>
2014-02-19 14:42:57 -0800

    front end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,31 +4,41 @@ class UsersController < ApplicationController
     end
     
     def add
-        result = User.add(params[:user], params[:password])
-        if result > 0
-            render :json => {:errCode => result, :count => result}
-        else render :json => {:errCode => result}
+        @result = User.add(params[:user], params[:password])
+        if @result > 0
+        #     render :json => {:errCode => @result, :count => @result}
+            @name = User.find_by_user(params[:user])
+            render :add
+        else
+        #    render :json => {:errCode => @result}
+            render :index
         end
     end
     
     def login
-        result = User.login(params[:user], params[:password])
-        if result > 0
-            render :json => {:errCode => 1, :count => result}
-        else render :json => {:errCode => result}
+        @result = User.login(params[:user], params[:password])
+        if @result > 0
+        #    render :json => {:errCode => 1, :count => @result}
+            @name = User.find_by_user(params[:user])
+            render :login
+        else
+        #    render :json => {:errCode => @result}
+            render :index
         end
     end
     
     def reset
         User.TESTAPI_resetFixture
-        render :json => {:errCode => 1}
+        #render :json => {:errCode => 1}
+        @result = -5
+        render :index
     end
     
     def unitTests
-	  	result = `ruby -Itest test/models/user_test.rb`
+	  	out = `ruby -Itest test/models/user_test.rb`
 	  	tests = result.scan(/\d+ tests/).second.split.first.to_i
 	  	fails = result.scan(/\d+ failures/).first.split.first.to_i
-	  	render json: {output: result, totalTests: tests, nrFailed: fails}
+	  	render json: {output: out, totalTests: tests, nrFailed: fails}
   	end
     
 end
